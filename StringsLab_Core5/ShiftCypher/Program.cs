b6df03ba1167f2eca932a9aa1a3ea45d3c8c0c45
LunarMoo <102759790+LunarMoo@users.noreply.github.com>
2022-04-12 18:04:31 -0700

    Update Program.cs

--- StringsLab_Core5/ShiftCypher/Program.cs
+++ StringsLab_Core5/ShiftCypher/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ShiftCypher
 {
@@ -6,7 +6,40 @@ namespace ShiftCypher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = "zoo";
+            int shift = 3;
+            string output = ShiftCypher(input, shift);
+            Console.WriteLine(output);
+        }
+        static string ShiftCypher(string s, int shift)
+        {
+            string encodedS = "";
+            foreach (char c in s)
+            {
+                if (char.IsLetter(c))
+                {
+                    int ascii = (int)c;
+                    ascii += shift;
+
+                    if (ascii >= (int)'a' && ascii < (int)'z')
+                    {
+                        char shiftedC = (char)ascii;
+                        encodedS += shiftedC;
+                    }
+                    else
+                    {
+                        char shiftedC = (char)(ascii - 26);
+                        encodedS += shiftedC;
+                    }
+
+                }
+                else
+                {
+                    encodedS += c;
+                }
+
+            }
+            return encodedS;
         }
     }
 }
